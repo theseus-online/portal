94a1176affdf3298a0c3efc33571508327365d75
lucklove <gnu.crazier@gmail.com>
2017-04-30 12:22:51 +0800

    Process to alpha

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,9 +2,9 @@
     <div>
         <Row style="margin-top: 25px;">
             <Col span="16" offset="4">
-                <Steps :current="0">
+                <Steps :current="1">
                     <Step title="preview" content="2017/4/21 basic concept"></Step>
-                    <Step title="alpha" content="2017/4/30 secure & form check"></Step>
+                    <Step title="alpha" content="2017/4/30 secure"></Step>
                     <Step title="beta" content="2017/5/20 network isolation & custom command"></Step>
                     <Step title="release" content="2017/7/20 more feature..."></Step>
                 </Steps>
@@ -12,7 +12,7 @@
         </Row>
         <Row style="margin-top: 25px;">
             <Col span="16" offset="4">
-                <Alert type="warning">This is a preview version for requirements collecting, do NOT try it in production envirment.</Alert>
+                <Alert type="warning">This is the alpha version for testing, do NOT use as production environment.</Alert>
                 <Alert type="info">See our <a href="http://blog.theseus.online" target="view_window">blog</a> for more information.</Alert>
             </Col>
         </Row>
